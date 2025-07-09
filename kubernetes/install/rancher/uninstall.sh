fda5547a6fa1da6239158c10ef981ab5edac4f3b
lukasdeveloperacc <lukasdeveloperacc@gmail.com>
2025-07-09 08:02:12 +0000

    change delete code order of cert manager

--- kubernetes/install/rancher/uninstall.sh
+++ kubernetes/install/rancher/uninstall.sh
@@ -3,6 +3,6 @@
 helm uninstall rancher --namespace cattle-system
 
 # Cert Manager
+kubectl delete -f https://github.com/cert-manager/cert-manager/releases/download/v1.18.2/cert-manager.crds.yaml
 helm uninstall cert-manager --namespace cert-manager
 kubectl delete namespace cattle-system
-kubectl delete -f https://github.com/cert-manager/cert-manager/releases/download/v1.18.2/cert-manager.crds.yaml
