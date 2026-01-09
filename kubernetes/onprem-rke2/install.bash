a200bf061014d8ac2272065867fdf45b8e498201
lukasdeveloperacc <lukasdeveloperacc@gmail.com>
2026-01-10 00:33:14 +0900

    update for installing rke2-server

--- kubernetes/onprem-rke2/install.bash
+++ kubernetes/onprem-rke2/install.bash
@@ -3,7 +3,7 @@
 # Version : https://github.com/rancher/rke2/releases
 curl -sfL https://get.rke2.io --output rke2_install.sh
 chmod +x ./rke2_install.sh
-INSTALL_RKE2_CHANNEL=v1.35.0+rke2r1 ./rke2_install.sh
+INSTALL_RKE2_CHANNEL=v1.34.3+rke2r1 ./rke2_install.sh
 
 systemctl enable rke2-server.service
 systemctl start rke2-server.service
