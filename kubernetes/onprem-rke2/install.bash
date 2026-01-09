43e99dee23ee57680f1b79358bb3dbc743f7e31e
lukasdeveloperacc <lukasdeveloperacc@gmail.com>
2026-01-10 00:20:59 +0900

    change to bash

--- kubernetes/onprem-rke2/install.bash
+++ kubernetes/onprem-rke2/install.bash
@@ -1,6 +1,11 @@
 #/bin/bash
 
 # Version : https://github.com/rancher/rke2/releases
-curl -sfL https://get.rke2.io | INSTALL_RKE2_VERSION=v1.35.0+rke2r1 sh -
+curl -sfL https://get.rke2.io --output rke2_install.sh
+chmod +x ./rke2_install.sh
+INSTALL_RKE2_CHANNEL=v1.35.0+rke2r1 ./rke2_install.sh
+
 systemctl enable rke2-server.service
 systemctl start rke2-server.service
+
+rm -rf ./rke2_install.sh
--- kubernetes/onprem-rke2/uninstall.bash
+++ kubernetes/onprem-rke2/uninstall.bash
@@ -1,4 +1,5 @@
 #/bin/bash
 
+rm -rf rke2_install.sh
 sh /usr/local/bin/rke2-killall.sh
 sh /usr/local/bin/rke2-uninstall.sh
